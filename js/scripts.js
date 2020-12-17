64a4029860fc7324972daca4e77888347fa59941
daisy-hunt <74766202+daisy-hunt@users.noreply.github.com>
2020-12-17 21:22:41 +0000

    Update nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log("Hi, Welcome to my portfolio site");
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 125 || document.documentElement.scrollTop > 125) {
+    document.getElementById("topnav").style.padding = "0px 0px";
+  } else {
+    document.getElementById("topnav").style.padding = "20px 0px";
+  }
+}
+  
