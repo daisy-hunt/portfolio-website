06ec3a47aa47d01656fc01280381c4dfcc115f36
daisy-hunt <74766202+daisy-hunt@users.noreply.github.com>
2021-01-07 20:00:48 +0000

    Update h2 text colour and logo

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function scrollNav() {
   if (document.body.scrollTop > 125 || document.documentElement.scrollTop > 125) {
     document.getElementById("topnav").style.padding = "0px 0px";
     document.getElementById("navlinks").style.fontSize = "0.9em";
-    document.getElementById("logo").style.maxWidth = "80px";
+    document.getElementById("logo").style.maxWidth = "90px";
     document.getElementById("sociallinks").style.fontSize = "0.9em";
     document.getElementById("topnav").style.boxShadow = "0px 0px 8px #c4c4c4";
   } else {
