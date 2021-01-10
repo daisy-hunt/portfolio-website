f3206878b702152048aa4609d47be553ff6441c4
daisy-hunt <74766202+daisy-hunt@users.noreply.github.com>
2021-01-10 12:23:25 +0000

    Update linting/accessibility errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,38 @@
-//Minimise Nav Bar
+// Minimise Nav Bar
 
-window.addEventListener("scroll", scrollNav);
+window.addEventListener('scroll', scrollNav)
 
-function scrollNav() {
+function scrollNav () {
   if (document.body.scrollTop > 125 || document.documentElement.scrollTop > 125) {
-    document.getElementById("topnav").style.padding = "0px 0px";
-    document.getElementById("navlinks").style.fontSize = "0.9em";
-    document.getElementById("logo").style.maxWidth = "90px";
-    document.getElementById("sociallinks").style.fontSize = "0.9em";
-    document.getElementById("topnav").style.boxShadow = "0px 0px 8px #c4c4c4";
+    document.getElementById('topnav').style.padding = '0px 0px'
+    document.getElementById('navlinks').style.fontSize = '0.9em'
+    document.getElementById('logo').style.maxWidth = '90px'
+    document.getElementById('sociallinks').style.fontSize = '0.9em'
+    document.getElementById('topnav').style.boxShadow = '0px 0px 8px #c4c4c4'
   } else {
-    document.getElementById("topnav").style.padding = "20px 0px";
-    document.getElementById("navlinks").style.fontSize = "1.1em";
-    document.getElementById("logo").style.maxWidth = "100px";
-    document.getElementById("sociallinks").style.fontSize = "1.1em";
-    document.getElementById("topnav").style.boxShadow = "0px 0px 0px #ffffff";
+    document.getElementById('topnav').style.padding = '20px 0px'
+    document.getElementById('navlinks').style.fontSize = '1.1em'
+    document.getElementById('logo').style.maxWidth = '100px'
+    document.getElementById('sociallinks').style.fontSize = '1.1em'
+    document.getElementById('topnav').style.boxShadow = '0px 0px 0px #ffffff'
   }
 }
 
-//Scroll Up Button
+// Scroll Up Button
 
-mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById('myBtn')
 
-window.addEventListener("scroll", scrollButton);
+window.addEventListener('scroll', scrollButton)
 
-function scrollButton() {
-if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-  mybutton.style.display = "block";
-} else {
-  mybutton.style.display = "none";
+function scrollButton () {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = 'block'
+  } else {
+    mybutton.style.display = 'none'
+  }
 }
-}
 
-function topFunction() {
-document.body.scrollTop = 0; // For Safari
-document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function topFunction () {
+  document.body.scrollTop = 0 // For Safari
+  document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
 }
